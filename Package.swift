281a8b2d4db5649f3c5f367ab595e1edb6935e2c
Amr Aboelela <amraboelela@gmail.com>
2016-12-28 16:57:42 -0800

    .Package(url: "https://github.com/amraboelela/CLevelDB", majorVersion: 1, minor: 0)

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftLevelDB",
     dependencies: [
-        .Package(url: "https://github.com/amraboelela/CLevelDB", majorVersion: 1)
+        .Package(url: "https://github.com/amraboelela/CLevelDB", majorVersion: 1, minor: 0)
     ]
 )
