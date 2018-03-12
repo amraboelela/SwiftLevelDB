337f587634580740fcd0eec29ba02295a65a5de8
Amr Aboelela <amraboelela@gmail.com>
2018-03-12 11:38:13 -0700

    converted it to swift 4.0

--- Package.swift
+++ Package.swift
@@ -1,10 +1,29 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftLevelDB",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftLevelDB",
+            targets: ["SwiftLevelDB"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/amraboelela/CLevelDB", majorVersion: 1, minor: 0)
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/amraboelela/CLevelDB", .branch("master"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SwiftLevelDB",
+            dependencies: []),
+        .testTarget(
+            name: "SwiftLevelDBTests",
+            dependencies: ["SwiftLevelDB"]),
     ]
 )
