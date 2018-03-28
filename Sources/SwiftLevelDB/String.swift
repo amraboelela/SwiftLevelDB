13e5baea549a66f2539a417a5a8bd69cda3f5df9
Amr Aboelela <amraboelela@gmail.com>
2018-03-28 10:50:29 -0700

    public var dataFromHexadecimal: Data?

--- Sources/SwiftLevelDB/String.swift
+++ Sources/SwiftLevelDB/String.swift
@@ -26,7 +26,21 @@ public extension String {
         #else
             return NSString(bytes: cString, length: length, encoding: String.Encoding.utf8.rawValue)! as String;
         #endif
-   }
+    }
+    
+    public var dataFromHexadecimal: Data? {
+        var data = Data(capacity: self.count / 2)
+        
+        let regex = try! NSRegularExpression(pattern: "[0-9a-f]{1,2}", options: .caseInsensitive)
+        regex.enumerateMatches(in: self, range: NSMakeRange(0, utf16.count)) { match, flags, stop in
+            let byteString = (self as NSString).substring(with: match!.range)
+            var num = UInt8(byteString, radix: 16)!
+            data.append(&num, count: 1)
+        }
+        
+        guard data.count > 0 else { return nil }
+        return data
+    }
 }
 
 func DLog(_ message: String, filename: String = #file, function: String = #function, line: Int = #line) {
