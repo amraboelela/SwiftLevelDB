706a2b6ad491c13af81c536eaa7a268eb4bf8782
Amr Aboelela <amraboelela@users.noreply.github.com>
2019-12-24 20:28:17 -0800

    #if os(Linux)

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@
 
 import PackageDescription
 
+#if os(Linux)
 let package = Package(
     name: "SwiftLevelDB",
     products: [
@@ -27,3 +28,29 @@ let package = Package(
             dependencies: ["SwiftLevelDB"]),
     ]
 )
+#else
+let package = Package(
+    name: "SwiftLevelDB",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftLevelDB",
+            targets: ["SwiftLevelDB"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        //.package(url: "https://github.com/amraboelela/CLevelDB", .branch("master"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SwiftLevelDB",
+            dependencies: []),
+        .testTarget(
+            name: "SwiftLevelDBTests",
+            dependencies: ["SwiftLevelDB"]),
+    ]
+)
+#endif
