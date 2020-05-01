bb025b53572e629f747794e957e2afe71491e1a1
Amr Aboelela <amraboelela@gmail.com>
2020-05-01 11:14:01 -0700

    Added CLevelDB.framework

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/amraboelela/CLevelDB", .branch("master"))
+        //.package(url: "https://github.com/amraboelela/CLevelDB", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,3 +27,7 @@ let package = Package(
             dependencies: ["SwiftLevelDB"]),
     ]
 )
+
+#if os(Linux)
+package.dependencies.append(.package(url: "https://github.com/amraboelela/CLevelDB", .branch("master")))
+#endif
