956656b628f38176d996ddbae4528367da69d7e8
Amr Aboelela <amraboelela@gmail.com>
2021-02-12 12:40:24 -0800

    using xcframework from CLevelDB package

--- Package.swift
+++ Package.swift
@@ -9,17 +9,16 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftLevelDB",
-            targets: ["SwiftLevelDB", "leveldb"])
+            targets: ["SwiftLevelDB"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/amraboelela/CLevelDB", .branch("xcframework")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "SwiftLevelDB", dependencies: ["CLevelDB"]),
-        .target(name: "CLevelDB", dependencies: ["leveldb"]),
+        //.target(name: "CLevelDB", dependencies: ["leveldb"]),
         .testTarget(name: "SwiftLevelDBTests", dependencies: ["SwiftLevelDB"]),
     ]
 )
@@ -33,9 +32,9 @@ package.targets.append(.target(
                         dependencies: [],
                         exclude: ["README"]))
 #else
-package.targets.append(.target(
+/*package.targets.append(.target(
                         name: "leveldb",
                         dependencies: ["leveldb_macos"],
                         exclude: ["README"]))
-package.targets.append(.target(name: "leveldb_macos", dependencies: []))
+package.targets.append(.target(name: "leveldb_macos", dependencies: []))*/
 #endif
