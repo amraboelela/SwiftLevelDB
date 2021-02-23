991060d2ba51e635db61faee702728af030770fe
Amr Aboelela <amraboelela@gmail.com>
2021-02-22 18:03:43 -0800

    Cleanup

--- Package.swift
+++ Package.swift
@@ -6,19 +6,14 @@ import PackageDescription
 let package = Package(
     name: "SwiftLevelDB",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftLevelDB",
             targets: ["SwiftLevelDB"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/amraboelela/leveldb", .branch("master")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "SwiftLevelDB", dependencies: ["CLevelDB"]),
         .target(name: "CLevelDB", dependencies: ["leveldb"]),
         .testTarget(name: "SwiftLevelDBTests", dependencies: ["SwiftLevelDB"]),
