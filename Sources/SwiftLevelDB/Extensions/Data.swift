53bb9212e2e2e137213e9d7041438281d09724dd
Amr Aboelela <amraboelela@gmail.com>
2022-07-06 18:36:36 -0700

    Update Data.swift

--- Sources/SwiftLevelDB/Extensions/Data.swift
+++ Sources/SwiftLevelDB/Extensions/Data.swift
@@ -28,6 +28,20 @@ extension Data {
     }
     
     public static func reportMemory() {
+        var taskInfo = task_vm_info_data_t()
+        var count = mach_msg_type_number_t(MemoryLayout<task_vm_info>.size) / 4
+        let result: kern_return_t = withUnsafeMutablePointer(to: &taskInfo) {
+            $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
+                task_info(mach_task_self_, task_flavor_t(TASK_VM_INFO), $0, &count)
+            }
+        }
+        let usedMb = Float(taskInfo.phys_footprint) / 1048576.0
+        let totalMb = Float(ProcessInfo.processInfo.physicalMemory) / 1048576.0
+        result != KERN_SUCCESS ? print("Memory used: ? of \(totalMb)") : print("Memory used: \(usedMb) of \(totalMb)")
+    }
+    
+    /*
+    public static func reportMemory() {
         var info = mach_task_basic_info()
         let MACH_TASK_BASIC_INFO_COUNT = MemoryLayout<mach_task_basic_info>.stride/MemoryLayout<natural_t>.stride
         var count = mach_msg_type_number_t(MACH_TASK_BASIC_INFO_COUNT)
@@ -45,5 +59,5 @@ extension Data {
             print("Error with task_info(): " +
                 (String(cString: mach_error_string(kerr), encoding: String.Encoding.ascii) ?? "unknown error"))
         }
-    }
+    }*/
 }
